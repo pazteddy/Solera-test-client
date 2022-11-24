5aa517ebb05939226d8d0bbe93cdefe3a930ed38
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 17:11:02 -0500

    Added styles global to assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import { Global } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { global, reset } from "./assets/global";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Global styles={global} />
+    <Global styles={reset} />
+
     <App />
   </React.StrictMode>
 );
