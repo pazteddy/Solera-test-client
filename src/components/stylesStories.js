31a888eac803ca71b739dfed941f2f9fe1ba4dfe
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 23:05:24 -0500

    Added padding to ul and ol

--- src/components/stylesStories.js
+++ src/components/stylesStories.js
@@ -3,6 +3,23 @@ import { colors } from "../assets/colors";
 import { fonts } from "../assets/typography";
 
 export const Wrapper = styled.div`
+  ul,
+  ol {
+    padding: 0;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  *::-webkit-scrollbar {
+    width: 0.3rem;
+  }
+
   * {
     font-family: ${fonts.primary};
     color: ${colors.primary};
