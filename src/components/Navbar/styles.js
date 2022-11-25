8311c908bbe507f18c44f4e7216782d77babfe52
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 16:14:55 -0500

    Created base styles to  tags

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+export const Nav = styled.nav``;
+
+export const Ul = styled.ul``;
+
+export const Link = styled(NavLink)``;
