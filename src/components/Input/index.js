52e4c1b7024724e783bc18e4de674c7fbb0cdb27
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 20:51:29 -0500

    Added conditional of type textarea

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,6 +7,25 @@ function Input(props) {
 
   const state = { ...props, stateInput: stateInput };
 
+  if (props.type === "textarea") {
+    return (
+      <S.Textarea
+        {...state}
+        rows="4"
+        onBlur={(e) => {
+          if (e.target.value !== "") {
+            setStateInput(true);
+          } else {
+            setStateInput(false);
+          }
+        }}
+        onFocus={() => {
+          setStateInput(true);
+        }}
+      />
+    );
+  }
+
   return (
     <S.Input
       {...state}
