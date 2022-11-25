5fd67ce507c879b92c1ec566a27cbe7920a0104b
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 16:32:59 -0500

    Union of the components for sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Menu from "./Layout/Menu";
+import MenuItem from "./Layout/MenuItem";
+import Navegation from "./Layout/Navegation";
 
-function Navbar() {
-  return <div>Navbar</div>;
+function Navbar({ data }) {
+  return (
+    <Navegation>
+      <Menu>
+        {data.map((item, i) => {
+          return (
+            <MenuItem key={i} to={item.to}>
+              {item.name}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </Navegation>
+  );
 }
 
 export default Navbar;
