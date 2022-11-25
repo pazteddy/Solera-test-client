59a6be7b40afd95ac01af7d7111502f792ccd428
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 20:52:14 -0500

    Added styles for the type textarea

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { colors } from "../../assets/colors";
 
 export const Input = styled.input`
+  padding: 0 10px;
   border: 1px solid #ccc;
   width: 100%;
   height: 40px;
@@ -10,4 +11,15 @@ export const Input = styled.input`
     stateInput &&
     `   border: 1px solid ${colors.secondary};
         outline: none;`}
+`;
+
+export const Textarea = styled.textarea`
+  padding: 10px;
+  border: 1px solid #ccc;
+  width: 100%;
+  border-radius: 5px;
+  ${({ stateInput }) =>
+    stateInput &&
+    `   border: 1px solid ${colors.secondary};
+    outline: none;`}
 `;
