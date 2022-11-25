644ba4673782038a51a725daa2269beb10a7b928
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 17:00:28 -0500

    Added stylos to component sidebar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,8 +1,44 @@
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
+import { colors } from "../../assets/colors";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  width: 100%;
+  height: 50px;
+  background-color: ${colors.backgroundSecondary};
+  display: flex;
+  align-items: center;
+  padding: 0 2rem;
+`;
 
-export const Ul = styled.ul``;
+export const Ul = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+`;
 
-export const Link = styled(NavLink)``;
+export const Link = styled(NavLink)`
+  position: relative;
+  color: ${colors.gray};
+  &::after {
+    content: "";
+    left: 0;
+    bottom: -0.25rem;
+    width: 100%;
+    height: 2px;
+    border-radius: 5px;
+    background-color: ${colors.secondary};
+  }
+
+  &.active {
+    font-weight: 500;
+  }
+
+  &.active,
+  &:hover {
+    color: ${colors.secondary};
+  }
+  &.active::after {
+    position: absolute;
+  }
+`;
