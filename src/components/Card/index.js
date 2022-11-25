ae6980050350c0df2c1e4b3af7ee19c25d6e1bf8
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 19:48:16 -0500

    Created component Card with functions, protype and defultProps

---
+++ src/components/Card/index.js
@@ -0,0 +1,55 @@
+import * as S from "./styles";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+function Card({ title, description, ...props }) {
+  const [remove, setRemove] = useState(false);
+  const [edit, setEdit] = useState(false);
+
+  const handleRemove = () => {
+    setRemove(true);
+    setTimeout(() => {
+      setRemove(false);
+      props.remove(props.id);
+    }, 2000);
+  };
+
+  const handleEdit = () => {
+    setEdit(true);
+    setTimeout(() => {
+      setEdit(false);
+      props.edit(props.id);
+    }, 1000);
+  };
+
+  const stateRemove = {
+    remove: remove,
+    edit: edit,
+  };
+  return (
+    <S.Wrapper {...stateRemove}>
+      <S.Content>
+        <S.Title>{title}</S.Title>
+        <S.Description>{description}</S.Description>
+      </S.Content>
+      <S.Actions>
+        <S.Action onClick={handleEdit}>Edit</S.Action>
+        <S.Action onClick={handleRemove}>Remove</S.Action>
+      </S.Actions>
+    </S.Wrapper>
+  );
+}
+
+Card.prototype = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  edit: PropTypes.func.isRequired,
+  remove: PropTypes.func.isRequired,
+};
+
+Card.defaultProps = {
+  remove: () => {},
+  edit: () => {},
+};
+
+export default Card;
