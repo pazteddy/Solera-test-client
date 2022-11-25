acdaa2e9d35ec8b22aeb6406834d284fdc1bb96d
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 17:31:22 -0500

    Added plus const colors

--- src/assets/colors/index.js
+++ src/assets/colors/index.js
@@ -1,6 +1,7 @@
 export const colors = {
   background: "#ffffff",
-  backgroundSecondary: "#e6f0f5", //f5f5f5
+  backgroundSecondary: "#2a2a2a", //#f5f5f5, #e6f0f5
+  backgroundTertiary: "#e6f0f5",
   white: "#ffffff",
   primary: "#000000",
   secondary: "#38a7fb",
