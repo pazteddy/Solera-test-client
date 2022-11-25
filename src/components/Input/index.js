df0709f2d7346b4c5d306c8b9239140ba6765657
k3v1n <kevinjhosepct@gmail.com>
2022-11-24 20:33:08 -0500

    Created Input component plus stateInput

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import * as S from "./styles";
+
 function Input(props) {
-  return <input {...props} />;
+  const [stateInput, setStateInput] = useState(false);
+
+  const state = { ...props, stateInput: stateInput };
+
+  return (
+    <S.Input
+      {...state}
+      onBlur={(e) => {
+        if (e.target.value !== "") {
+          setStateInput(true);
+        } else {
+          setStateInput(false);
+        }
+      }}
+      onFocus={() => {
+        setStateInput(true);
+      }}
+    />
+  );
 }
 
 export default Input;
