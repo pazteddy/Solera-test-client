95a7191ffe62d6c49be1c4068e373d0e13d8f0ae
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 18:53:11 -0500

    Added remove and update service

--- src/context/servicesReducer.js
+++ src/context/servicesReducer.js
@@ -4,6 +4,16 @@ export const servicesReducer = (state, action) => {
       return action.payload;
     case "addService":
       return [...state, action.payload];
+    case "removeService":
+      return state.filter((service) => service.id !== action.payload);
+    case "updateService":
+      return state.map((service) => {
+        if (service.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return service;
+        }
+      });
     default:
       return { ...state };
   }
