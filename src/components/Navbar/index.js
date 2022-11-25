a69c1e780a942961dcbae593e378fc895e1003b0
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 17:59:29 -0500

    Added Logo, propTypes and defultProps

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import Menu from "./Layout/Menu";
 import MenuItem from "./Layout/MenuItem";
 import Navegation from "./Layout/Navegation";
+import PropTypes from "prop-types";
+import { colors } from "../../assets/colors";
+import { Logo } from "./styles";
 
-function Navbar({ data }) {
+function Navbar({ data, logo, ...props }) {
   return (
-    <Navegation>
+    <Navegation {...props}>
+      <Logo>{logo && <img src={logo} alt="Solera-logo" />}</Logo>
       <Menu>
         {data.map((item, i) => {
           return (
@@ -18,5 +22,17 @@ function Navbar({ data }) {
     </Navegation>
   );
 }
+
+Navbar.propTypes = {
+  data: PropTypes.array.isRequired,
+  logo: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  data: [],
+  logo: "",
+  backgroundColor: colors.backgroundTertiary,
+};
 
 export default Navbar;
