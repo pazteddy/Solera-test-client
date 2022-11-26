6c30d54735d64a212fe2a3c42e798f9b8ce850c2
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 09:11:04 -0500

    Created styles and component Error

---
+++ src/components/Error/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ErrorStyle } from "./styles";
+
+function Error({ children }) {
+  return <ErrorStyle>{children}</ErrorStyle>;
+}
+
+export default Error;
---
+++ src/components/Error/styles.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { colors } from "../../assets/colors";
+
+export const ErrorStyle = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${colors.error};
+`;
