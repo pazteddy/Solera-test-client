584619a0d6df2555c0f92b40ad07bdb470bd0a11
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 13:41:55 -0500

    Modified key title for name

--- src/utils.js
+++ src/utils.js
@@ -3,37 +3,37 @@ import { colors } from "./assets/colors";
 export const services = [
   {
     id: 1,
-    title: "Electricidad",
+    name: "Electricidad",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     service: "Hogar",
   },
   {
     id: 2,
-    title: "Auxilio Mecánico",
+    name: "Auxilio Mecánico",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     service: "autos",
   },
   {
     id: 3,
-    title: "Chofer reemplazo",
+    name: "Chofer reemplazo",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     service: "autos",
   },
   {
     id: 4,
-    title: "Medico a domicilio",
+    name: "Medico a domicilio",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     service: "salud",
   },
   {
     id: 5,
-    title: "Ambulancia",
+    name: "Ambulancia",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     service: "salud",
   },
   {
     id: 6,
-    title: "Gasfitero",
+    name: "Gasfitero",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     animationIterationCount: "infinite",
     service: "hogar",
