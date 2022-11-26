ba5ab436c0355650c831d17a80dce9cc663d82d1
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 15:18:32 -0500

    Deleted console.log

--- src/context/formReducer.js
+++ src/context/formReducer.js
@@ -1,6 +1,4 @@
 export const formReducer = (state, action) => {
-  console.log(state);
-  console.log(action.payload);
   switch (action.type) {
     case "setForm":
       return { ...state, ...action.payload };
