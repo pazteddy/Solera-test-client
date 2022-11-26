c92d74c38cd2ba1232500ffd1cd7b8c5ba1cb4a5
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 11:21:16 -0500

    Created component Form

---
+++ src/components/Form/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { typesServicesArray } from "../../utils";
+import Button from "../Button";
+import Error from "../Error";
+import FormInput from "../FormInput";
+
+function Form() {
+  const [error, setError] = useState("");
+  const initialForm = {
+    name: "",
+    description: "",
+    service: "",
+  };
+  const [form, setForm] = useState(initialForm);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.name]: e.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (form.name === "" || form.description === "" || form.service === "") {
+      setError("Complete todos los campos");
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    } else {
+      console.log(form);
+      setForm(initialForm);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormInput
+        type="text"
+        label="Nombre"
+        name="name"
+        value={form.name}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange(e.target);
+        }}
+      />
+      <FormInput
+        type="textarea"
+        label="Description"
+        name="description"
+        value={form.description}
+        onChange={(e) => {
+          e.preventDefault();
+          handleChange(e.target);
+        }}
+      />
+      <FormInput
+        type="select"
+        label="Select service"
+        name="service"
+        value={form.service}
+        onChange={handleChange}
+        placeholder="Select one of the following services"
+        width="100%"
+        options={typesServicesArray}
+      />
+      {error && <Error>{error}</Error>}
+      <div>
+        <Button type="submit" label="Grabar" color="blue" />
+        <Button type="button" label="Cancelar" color="red" />
+      </div>
+    </form>
+  );
+}
+
+export default Form;
