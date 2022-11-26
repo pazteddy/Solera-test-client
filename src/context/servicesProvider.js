855b6c53664d14faa363458b1280c7c4585d8834
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 15:16:29 -0500

    Added state global to form

--- src/context/servicesProvider.js
+++ src/context/servicesProvider.js
@@ -2,17 +2,31 @@ import { useEffect, useReducer } from "react";
 import { useLocation } from "react-router";
 import { services } from "../utils";
 import { filterReducer } from "./filterReducer";
+import { formReducer } from "./formReducer";
 import { ServicesContext } from "./servicesContext";
 import { servicesReducer } from "./servicesReducer";
 
 export const ServicesProvider = ({ children }) => {
+  // State global  to services
   const [data, dispatch] = useReducer(
     servicesReducer,
     localStorage.getItem("data")
       ? JSON.parse(localStorage.getItem("data"))
       : services
   );
+
+  // State global to filter data from services
   const [dataFilter, dispatchFilter] = useReducer(filterReducer, []);
+
+  // State global  to Form
+  const initialForm = {
+    name: "",
+    description: "",
+    service: "",
+  };
+  const [form, dispatchForm] = useReducer(formReducer, initialForm);
+
+  // Get url
   let location = useLocation();
 
   useEffect(() => {
@@ -49,6 +63,9 @@ export const ServicesProvider = ({ children }) => {
     updateService,
     dataFilter,
     dispatchFilter,
+    initialForm,
+    form,
+    dispatchForm,
   };
   return (
     <ServicesContext.Provider value={values}>
