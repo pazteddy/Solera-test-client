59965397bee18732ff4a1aee988555bb0031199d
k3v1n <kevinjhosepct@gmail.com>
2022-11-25 19:19:31 -0500

    Added location for filter to the services

--- src/context/servicesProvider.js
+++ src/context/servicesProvider.js
@@ -1,14 +1,29 @@
 import { useEffect, useReducer } from "react";
+import { useLocation } from "react-router";
 import { services } from "../utils";
+import { filterReducer } from "./filterReducer";
 import { ServicesContext } from "./servicesContext";
 import { servicesReducer } from "./servicesReducer";
 
 export const ServicesProvider = ({ children }) => {
   const [data, dispatch] = useReducer(servicesReducer, []);
+  const [dataFilter, dispatchFilter] = useReducer(filterReducer, []);
+  let location = useLocation();
 
   useEffect(() => {
     dispatch({ type: "setServices", payload: services });
-  });
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname.slice(1) === "todos") {
+      dispatchFilter({ type: "setFilter", payload: data });
+    } else {
+      const servicesFiltered = data.filter(
+        (service) => service.service === location.pathname.slice(1)
+      );
+      dispatchFilter({ type: "setFilter", payload: servicesFiltered });
+    }
+  }, [location, data]);
 
   const addService = (service) => {
     dispatch({ type: "addService", payload: service });
@@ -22,7 +37,14 @@ export const ServicesProvider = ({ children }) => {
     dispatch({ type: "updateService", payload: service });
   };
 
-  const values = { data, addService, removeService, updateService };
+  const values = {
+    data,
+    addService,
+    removeService,
+    updateService,
+    dataFilter,
+    dispatchFilter,
+  };
   return (
     <ServicesContext.Provider value={values}>
       {children}
