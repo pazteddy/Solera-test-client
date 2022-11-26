2f5d97d0bc09e2036569213f535b52b66e1594ec
k3v1n <kevinjhosepct@gmail.com>
2022-11-26 13:46:59 -0500

    Added components and styles in line with styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { css } from "@emotion/css";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+
+import Logo from "./assets/images/logo.png";
+import { types } from "./utils";
+import { useServices } from "./context";
+import Form from "./components/Form";
+
 function App() {
-  return <></>;
+  const { dataFilter, removeService } = useServices();
+  const handleRemove = (id) => {
+    removeService(id);
+  };
+  return (
+    <>
+      <h1
+        className={css`
+          text-align: center;
+        `}
+      >
+        Services
+      </h1>
+      <Navbar data={types} backgroundColor="black" logo={Logo} />
+      <div
+        className={css`
+          padding: 20px;
+          display: grid;
+          grid-template-columns: 2fr 1fr;
+        `}
+      >
+        <div
+          className={css`
+            padding: 20px;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+          `}
+        >
+          {dataFilter.map((service) => {
+            return <Card key={service.id} {...service} remove={handleRemove} />;
+          })}
+        </div>
+
+        {/* Form */}
+
+        <Form />
+      </div>
+    </>
+  );
 }
 
 export default App;
