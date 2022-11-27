ea8c5de950020d78c8a61a718a911fe8f8daf87c
k3v1n <kevinjhosepct@gmail.com>
2022-11-27 12:11:35 -0500

    Modified Route and added pages

--- src/App.js
+++ src/App.js
@@ -1,75 +1,13 @@
-import { css } from "@emotion/css";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
-
-import Logo from "./assets/images/logo.png";
-import { types } from "./utils";
-import { useServices } from "./context";
-import Form from "./components/Form";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const { dataFilter, removeService, data, dispatchForm } = useServices();
-  const handleRemove = (id) => {
-    removeService(id);
-  };
-
-  const handleEdit = (idCard) => {
-    // console.log(id);
-    console.log(idCard);
-    const { id, name, description, service } = data.find(
-      (service) => service.id === idCard
-    );
-
-    dispatchForm({
-      type: "setForm",
-      payload: {
-        id: id,
-        name: name,
-        description: description,
-        service: service,
-      },
-    });
-  };
   return (
     <>
-      <h1
-        className={css`
-          text-align: center;
-        `}
-      >
-        Services
-      </h1>
-      <Navbar data={types} backgroundColor="black" logo={Logo} />
-      <div
-        className={css`
-          padding: 20px;
-          display: grid;
-          grid-template-columns: 2fr 1fr;
-        `}
-      >
-        <div
-          className={css`
-            padding: 20px;
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-          `}
-        >
-          {dataFilter.map((service) => {
-            return (
-              <Card
-                key={service.id}
-                {...service}
-                remove={handleRemove}
-                edit={handleEdit}
-              />
-            );
-          })}
-        </div>
-
-        {/* Form */}
-
-        <Form />
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/todos" />} />
+      </Routes>
+      <Home />
     </>
   );
 }
