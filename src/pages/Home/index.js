7c185b50984bbd19e0be30d4231d1edfdeb9cbe6
k3v1n <kevinjhosepct@gmail.com>
2022-11-27 19:10:23 -0500

    Modified structure and added Content style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,18 @@ import Form from "../../components/Form";
 import ListCard from "../../components/ListCard";
 import Navbar from "../../components/Navbar";
 import { types } from "../../utils";
-import { Container, Title } from "./styles";
+import { Container, Content, Title } from "./styles";
 
 function Home() {
   return (
     <>
-      <Title>Servicios</Title>
       <Navbar data={types} />
       <Container>
-        <ListCard />
-        <Form />
+        <Title>Servicios</Title>
+        <Content>
+          <ListCard />
+          <Form />
+        </Content>
       </Container>
     </>
   );
