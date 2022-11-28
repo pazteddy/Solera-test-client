67951d13632092c91845eae6225c830c9edcf47f
k3v1n <kevinjhosepct@gmail.com>
2022-11-27 19:58:56 -0500

    Added to the component Navbar logo and bgColor

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,12 @@ import ListCard from "../../components/ListCard";
 import Navbar from "../../components/Navbar";
 import { types } from "../../utils";
 import { Container, Content, Title } from "./styles";
+import Logo from "../../assets/images/logo.png";
 
 function Home() {
   return (
     <>
-      <Navbar data={types} />
+      <Navbar data={types} logo={Logo} backgroundColor="#000" />
       <Container>
         <Title>Servicios</Title>
         <Content>
